02fb0110a31ec0a225c4c5c6444c27eb66d4178d
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-05 20:04:06 -0300

    aula depois das férias

---
+++ subfuncao.py
@@ -0,0 +1,20 @@
+def saudacoes(teste):
+    print(teste)
+
+saudacoes("lalalalalal")
+
+def media():
+    nota1 = 10
+    nota2 = 10
+    nota3 = 10
+    media = (nota1 + nota2 + nota3) / 3
+    print(f"A média é {media}")
+
+media()
+
+
+def apresenta_media(nome:str, nota1:float, nota2:float):
+    media = (nota1 + nota2) /2
+    print(f"Olá {nome}, a média é: {media}")
+
+apresenta_media("Brenno", 10, 8)
