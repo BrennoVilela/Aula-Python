ab667390469af03530d372bc3f1feacfd91b4e92
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-05 20:27:29 -0300

    add

--- subfuncao.py
+++ subfuncao.py
@@ -1,20 +1,29 @@
-def saudacoes(teste):
-    print(teste)
+# def saudacoes(teste):
+#     print(teste)
 
-saudacoes("lalalalalal")
+# saudacoes("lalalalalal")
 
-def media():
-    nota1 = 10
-    nota2 = 10
-    nota3 = 10
-    media = (nota1 + nota2 + nota3) / 3
-    print(f"A média é {media}")
+# def media():
+#     nota1 = 10
+#     nota2 = 10
+#     nota3 = 10
+#     media = (nota1 + nota2 + nota3) / 3
+#     print(f"A média é {media}")
 
-media()
+# media()
 
 
-def apresenta_media(nome:str, nota1:float, nota2:float):
-    media = (nota1 + nota2) /2
-    print(f"Olá {nome}, a média é: {media}")
+# def apresenta_media(nome:str, nota1:float, nota2:float):
+#     media = (nota1 + nota2) /2
+#     print(f"Olá {nome}, a média é: {media}")
 
-apresenta_media("Brenno", 10, 8)
+# apresenta_media("Brenno", 10, 8)
+
+def media_funcao(*args):
+    soma = 0
+    for nota in args:
+        soma += nota
+    media = soma / len(args)
+    return media
+
+media_funcao(10, 30, 20, 5, 2, 3, 3)
