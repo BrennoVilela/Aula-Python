a73d8c4814b3697fb292ff30c6330296b1bc4662
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-09 20:46:22 -0300

    finalizando aula de python

--- segundo semestre/arquivo.py
+++ segundo semestre/arquivo.py
@@ -1,12 +1,24 @@
+# Abrindo o arquivo para escrever coisas nele
 arq = open("arquivo.txt", "w")
-
 arq.write("Gravando a linha 1 no arquivo\n")
 arq.write("Gravando a linha 2 no arquivo\n")
 arq.write("Gravando a linha 3 no arquivo\n")
-
 arq.close()
 
-
+# Abrindo o arquivo para ler as coisas que estão nele
 reader = open("arquivo.txt", "r")
 print(reader.read())
 reader.close()
+
+#Abrindo o arquivo para editar ele
+appender = open("arquivo.txt", "a")
+appender.write("\n")
+appender.write("Gravando a linha 8")
+appender.close()
+
+#Abrindo o arquivo para poder ler ele de novo
+print(reader.read())
+reader.close()
+
+#
+arq = open("arquivo2.txt", "x")
